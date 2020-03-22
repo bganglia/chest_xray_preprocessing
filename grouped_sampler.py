d66179b1e54738cc08d5b8a8e3eb7def4fd66b2a
bganglia <bganglia892@gmail.com>
2020-03-22 16:52:47 -0400

    add grouped sampler

---
+++ grouped_sampler.py
@@ -0,0 +1,7 @@
+from torch import random_split
+
+def grouped_split(dataset, groups, lengths):
+    unique_groups = groups.unique()
+    selected_groups = unique_groups(dataset)
+    out = [dataset[groups==subset] for subset in dataset]
+    return out
